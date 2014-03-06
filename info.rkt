f21d2a5e9b01f2535c3546f0abf90f24dc14cda2
Ismael Figueroa <ifigueroap@gmail.com>
2014-03-06 14:06:20 -0300

    Initial import

---
+++ info.rkt
@@ -0,0 +1,14 @@
+#lang setup/infotab
+
+(define blurb '("DeinProgramm - QuickCheck"))
+(define primary-file "quickcheck.rkt")
+
+(define compile-omit-files
+  '("examples.scm"
+    "packages.scm"
+    "quickcheck-test.scm"
+    "quickcheck.scm"
+    "random.scm"))
+
+
+
