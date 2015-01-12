664d106698135ad3ddbc9cb083be7ec16fd7ddef
Asumu Takikawa <asumu@ccs.neu.edu>
2015-01-12 12:08:37 -0500

    Fix build-deps for package

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define blurb '("DeinProgramm - QuickCheck"))
 (define primary-file "main.rkt")
 (define deps '("base"))
-(define build-deps '("scribble-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/quickcheck.scrbl")))
 
 (define compile-omit-files
