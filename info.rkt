d5301c8867fff508674519f20f16850a7f15d195
Jack Firth <jackhfirth@gmail.com>
2015-10-14 11:53:58 -0700

    Move `rack-commands` to collection info to fix it

--- info.rkt
+++ info.rkt
@@ -9,9 +9,3 @@
 
 (define build-deps '("scribble-lib"
                      "racket-doc"))
-
-(define raco-commands
-  '(("quickcheck"
-     quickcheck/raco-quickcheck
-     "autogenerate property test cases"
-     25)))
--- quickcheck/info.rkt
+++ quickcheck/info.rkt
@@ -2,3 +2,9 @@
 
 (define name "quickcheck")
 (define scribblings '(("scribblings/quickcheck.scrbl")))
+
+(define raco-commands
+  '(("quickcheck"
+     raco-quickcheck
+     "autogenerate property test cases"
+     25)))
